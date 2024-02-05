2d988c565481496a81dfb10f8f997120dae8e38c
EtiennePONS <etienne.pons@hotmail.fr>
2024-02-05 15:38:24 +0100

    mise à jour des villes non repertoriees

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let container = document.querySelector(".container");
 let search = document.querySelector(".search-box button");
 let weatherBox = document.querySelector(".weather-box");
-let weatherDetails = document.querySelector(".weatherdetails");
+let weatherDetails = document.querySelector(".weather-details");
+let error404 = document.querySelector(".not-found");
+let cityHide = document.querySelector(".city-hide");
 
 search.addEventListener("click", () => {
   const city = document.querySelector(".search-box input").value;
@@ -11,18 +13,36 @@ search.addEventListener("click", () => {
   fetch(`https://www.prevision-meteo.ch/services/json/${city}`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data.current_condition.tmp);
-      let { current_condition } = data;
+      if (data.errors) {
+        cityHide.textContent = city;
+        container.style.height = "400px";
+        weatherBox.classList.remove("active");
+        weatherDetails.classList.remove("active");
+        error404.classList.add("active");
+        return;
+      }
+
       let temperature = document.querySelector(".weather-box .temperature");
       let vitesseVent = document.querySelector(".weather-details .info-wind");
       let humidite = document.querySelector(".weather-details .info-humidity");
       let conditionMeteo = document.querySelector(".weather-box .description");
       let image = document.querySelector(".weather-box img");
 
-      temperature.innerHTML = `${current_condition.tmp}<span>C°</span>`;
-      vitesseVent.innerHTML = `${current_condition.wnd_spd}<span>Km/h</span>`;
-      humidite.innerHTML = `${current_condition.humidity}<span>%</span>`;
-      conditionMeteo.innerHTML = current_condition.condition;
-      image.src = current_condition.icon_big;
+      if (cityHide.textContent == city) {
+        return;
+      } else {
+        cityHide.textContent = city;
+        container.style.height = "555px";
+        container.classList.add("active");
+        weatherBox.classList.add("active");
+        weatherDetails.classList.add("active");
+        error404.classList.remove("active");
+
+        temperature.innerHTML = `${data.current_condition.tmp}<span>C°</span>`;
+        vitesseVent.innerHTML = `${data.current_condition.wnd_spd}<span>Km/h</span>`;
+        humidite.innerHTML = `${data.current_condition.humidity}<span>%</span>`;
+        conditionMeteo.innerHTML = data.current_condition.condition;
+        image.src = data.current_condition.icon_big;
+      }
     });
 });
